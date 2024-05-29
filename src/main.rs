01da87a71887f9299a75164735343e5d2cedb0f7
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2024-05-29 19:06:28 +0530

    enh: Introduced CLI commands for enhanced functionality

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,72 @@
 mod macros;
-mod versions;
+// mod versions;
 mod install;
 mod uninstall;
 
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
 use color_eyre::eyre::Result;
 use dirs::home_dir;
 
 use install::install;
 use uninstall::uninstall;
-use versions::get_latest_release;
+// use versions::get_latest_release;
+
+/// CLI structure to parse command-line arguments.
+#[derive(Parser)]
+#[command(
+    version,
+    about,
+    long_about = None
+)]
+struct Cli {
+    /// Commands.
+    #[command(subcommand)]
+    command: Commands,
+    
+    /// Sets a custom installation path.
+    #[arg(long, value_name = "PATH")]
+    path: Option<PathBuf>,
+
+    /// Flags.
+    #[arg(short, long)]
+    verbose: bool,
+}
+
+/// Enum representing different commands.
+#[derive(Subcommand)]
+enum Commands {
+    /// Install Amethyst.
+    Install,
+    /// Uninstall Amethyst.
+    Uninstall,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Parse command-line arguments into the CLI structure.
+    let cli = Cli::parse();
 
-    // Get the latest release
+    // Print the specified path, if provided.
+    println!("Specified Path: {:?}", cli.path.as_deref());
 
-    let latest_release = get_latest_release().await?;
-    println!("Latest version: v{}", latest_release.0);
-    
-    // Install Amethyst Core & Panel.
-    
-    let path = home_dir()
-        .expect("Could not find home directory")
-        .join(".amethyst");
-    install(&path);
+    // Print verbose mode status.
+    // println!("Verbose: {:?}", cli.verbose);
 
-    // Uninstalling Amethyst Data with path.
-    
-    uninstall()?;
+    // Match the command and execute corresponding logic.
+    match &cli.command {
+        Commands::Install => {
+            // Install Amethyst Core & Panel.
+            let path = home_dir()
+                .expect("Could not find home directory")
+                .join(".amethyst");
+            install(&path);
+        }
+        Commands::Uninstall => {
+            // Uninstall Amethyst Core & Panel.
+            uninstall()?;
+        }
+    }
+
     Ok(())
 }
